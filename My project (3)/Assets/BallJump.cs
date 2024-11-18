bddd142653bf9974b845cadab98f253492dcdbf3
ITHUB\Student <alexanderyugay2007@gmail.com>
2024-11-18 12:28:11 +0300

    2nd stage

--- My project (3)/Assets/BallJump.cs
+++ My project (3)/Assets/BallJump.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,18 +6,46 @@ using UnityEngine;
 public class BallJump : MonoBehaviour
 {
     [SerializeField] Material[] materials;
+    [SerializeField] int speed;    
     private MeshRenderer mesh;
+    private Rigidbody rb;
+    private bool changeColor = true;
+    private float timer;
 
     private void Start()
     {
         mesh = GetComponent<MeshRenderer>();
+        rb = GetComponent<Rigidbody>();
+    }
+    private void Update()
+    {
+        timer += Time.deltaTime;
+        if (Mathf.Abs(rb.velocity.y) < speed && changeColor == true) 
+        { 
+            timer = 0;
+            changeColor = false;
+            ChangeMaterial();
+        }
+        if(Mathf.Abs(rb.velocity.y) > speed)
+        {
+            changeColor = true;
+        }
+        if(changeColor == false)
+        {
+            Debug.Log(timer);
+        }
+    }
+
+    private void ChangeMaterial()
+    {
+        mesh.material = materials[UnityEngine.Random.Range(0, materials.Length)];
     }
 
     private void OnCollisionEnter(Collision collision)
     {
         if(collision.gameObject.CompareTag("Ground"))
         {
-            mesh.material = materials[Random.Range(0, materials.Length)];
+            ChangeMaterial();
         }
     }
 }
